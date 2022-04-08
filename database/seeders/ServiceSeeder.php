5b954d90ae4f5735edbcc8b3eefcf5f9ca0d1495
nicola <sieben.nicola@gmail.com>
2022-04-08 16:02:07 +0200

    plop

--- database/seeders/ServiceSeeder.php
+++ database/seeders/ServiceSeeder.php
@@ -17,9 +17,29 @@ class ServiceSeeder extends Seeder
     {
         //
 		DB::table("services")->insert([
-			'icone' => '',
-			'title' => '',
-			'description' => '',
+			'icone' => 'service-icon-01.png',
+			'title' => 'Creative Ideas',
+			'description' => 'You can download and use this EduWell Template for your teaching and learning stuffs.',
+		]);
+        DB::table("services")->insert([
+			'icone' => 'service-icon-02.png',
+			'title' => 'Useful Tricks',
+			'description' => 'EduWell is the professional HTML5 template for your school or university websites.',
+		]);
+        DB::table("services")->insert([
+			'icone' => 'service-icon-03.png',
+			'title' => 'Ready Target',
+			'description' => 'Please tell your friends about the best CSS template website that is TemplateMo.',
+		]);
+        DB::table("services")->insert([
+			'icone' => 'service-icon-04.png',
+			'title' => 'Technology',
+			'description' => 'Aenean bibendum consectetur ex eu porttitor. Pellentesque id ultrices metus.',
+		]);
+        DB::table("services")->insert([
+			'icone' => 'service-icon-01.png',
+			'title' => 'Creative Ideas',
+			'description' => 'EduWell is the professional HTML5 template for your school or university websites.',
 		]);
     }
 }
