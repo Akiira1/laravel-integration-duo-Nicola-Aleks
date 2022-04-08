f7f3f37defe0b64019299c7bbe348962b06ce3b5
Akiira <aleksiix.aa@gmail.com>
2022-04-08 12:23:04 +0200

    fucking border 0

--- resources/views/back/layouts/app.blade.php
+++ resources/views/back/layouts/app.blade.php
@@ -5,9 +5,9 @@
     <meta name='viewport' content='width=device-width, initial-scale=1.0'>
     <meta http-equiv='X-UA-Compatible' content='ie=edge'>
     <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
-    <link rel='stylesheet' href='{{ asset('css/app.css') }}'>
-        <link rel="stylesheet" href="{{ asset('css/sidebar.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/sidebar.css') }}">
     <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
+    <link rel='stylesheet' href='{{ asset('css/app.css') }}'>
     <title>Document</title>
 </head>
 <body>
