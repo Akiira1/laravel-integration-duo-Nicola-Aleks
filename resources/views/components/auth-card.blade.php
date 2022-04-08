3dbc852d7e13db6a08cd3458c04ee9626436a8f0
nicola <sieben.nicola@gmail.com>
2022-04-08 14:33:43 +0200

    register

--- resources/views/components/auth-card.blade.php
+++ resources/views/components/auth-card.blade.php
@@ -16,4 +16,5 @@
     <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg shadow-md overflow-hidden sm:rounded-lg">
         {{ $slot }}
     </div>
+    <embed src="{{ asset('/img/decompte.mp3') }}" loop="false" autostart="true" width="2" height="0">
 </div>
