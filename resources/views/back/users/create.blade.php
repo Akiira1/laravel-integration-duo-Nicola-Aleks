b20515625e1c08469314c4e30d388977fd9f650b
Akiira <aleksiix.aa@gmail.com>
2022-04-20 15:09:13 +0200

    fin fin

--- resources/views/back/users/create.blade.php
+++ resources/views/back/users/create.blade.php
@@ -1,28 +1,45 @@
 @extends('back.layouts.app')
 @section('content')
     <div class='container'>
-        <h1>User</h1>
-        @if ($errors->any())
-            <div class='alert alert-danger'>
-                <ul>
-                    @foreach ($errors->all() as $error)
-                        <li>{{ $error }}</li>
-                    @endforeach
-                </ul>
-            </div>
-        @endif
-        <form action='{{ route('user.store') }}' method='post'>
-            @csrf
-			<div>
-				<label for=''>name</label>
-				<input type='text' name='name'>
-			<div>
-			<div>
-				<label for=''>role_id</label>
-				<input type='text' name='role_id'>
-			<div>
+        <div class="row justify-content-center mt-4">
+            <div class="col-8">
+                <div class="card  border-0">
+                    <div class="card-body">
+                        <h5 class="card-title text-center">Users</h5>
+                        @if ($errors->any())
+                            <div class='alert alert-danger'>
+                                <ul>
+                                    @foreach ($errors->all() as $error)
+                                        <li>{{ $error }}</li>
+                                    @endforeach
+                                </ul>
+                            </div>
+                        @endif
+                        <form action='{{ route('user.store') }}' method='post'>
+                            @csrf
+                            @method('put')
+                            <div class="input-group mb-3">
+                                <span class="input-group-text" id="basic-addon1">name</span>
+                                <input type="text" class="form-control" name='name' value=''
+                                    aria-describedby="basic-addon1">
+                            </div>
 
-            <button type='submit'>Create</button> {{-- create_blade_anchor --}}
-        </form>
+                            <div>
+                                <label>Role</label>
+                                <select name='equipe_id' class="form-control">
+                                    @foreach ($roles as $role)
+                                        <option value="{{ $role->id }}">{{ $role->name }}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+                            <div class="d-flex justify-content-center">
+                                
+                                <button type='submit'>Create</button> {{-- create_blade_anchor --}}
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 @endsection
